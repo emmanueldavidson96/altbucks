0ffdfa023cfbe668e705ae12e408850021dcf2c4
Emmanuel <emmanueldavidson524@gmail.com>
2024-10-29 12:33:16 +0100

    Downgraded Nextjs

---
+++ src/app/about_us/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='h-screen w-screen bg-white'>
+        <h2 className='text-black text-4xl'>ABOUT</h2>
+    </div>
+  )
+}
