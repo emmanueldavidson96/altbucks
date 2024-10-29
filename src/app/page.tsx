39dfa7ec29c62105e5a21bd6d302909da90f2687
Emmanuel <emmanueldavidson524@gmail.com>
2024-10-29 11:29:37 +0100

    Fixed problems

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-white">
-      <h1>ALTBUCKS PROJECT HOMEPAGE</h1>
+      <h1 className="text-4xl text-black">ALTBUCKS PROJECT HOMEPAGE</h1>
     </div>
   );
 }
