840186ef492b1c70b5dfac0a0c7a0a87908c94b8
Linet  Mukai <151999722+LinetMutuku@users.noreply.github.com>
2024-11-16 11:45:03 +0300

    correct typo

--- src/app/components/About_Components/ViewReviews.tsx
+++ src/app/components/About_Components/ViewReviews.tsx
@@ -14,81 +14,81 @@ import imgFive from "../../../../public/assets/Image (3).png"
 import imgSix from "../../../../public/assets/Image (2).png"
 
 function ViewReviews() {
-  return (
-    <div className='bg-[#F1F2F4] h-[600px] w-full flex flex-col items-center justify-center gap-16 relative'>
-        <h2 className='font-semibold text-3xl text-black tracking-wide'>Reviewed by the community</h2>
-        <div className='h-[60px] w-[60px] rounded-full bg-[#377BFF] flex items-center justify-center absolute top-[165px] z-20'>
-            <Image src={quoteImg} alt='' className='w-[30px] h-[30px] object-cover' />            
+    return (
+        <div className='bg-[#F1F2F4] h-[600px] w-full flex flex-col items-center justify-center gap-16 relative'>
+            <h2 className='font-semibold text-3xl text-black tracking-wide'>Reviewed by the community</h2>
+            <div className='h-[60px] w-[60px] rounded-full bg-[#377BFF] flex items-center justify-center absolute top-[165px] z-20'>
+                <Image src={quoteImg} alt='' className='w-[30px] h-[30px] object-cover' />
+            </div>
+            <Image src={imgOne} alt='' className='absolute w-[50px] h-[50px] object-cover bottom-16 left-56'/>
+            <Image src={imgTwo} alt='' className='absolute w-[50px] h-[50px] object-cover bottom-60 left-36'/>
+            <Image src={imgThree} alt='' className='absolute w-[50px] h-[50px] object-cover top-24 left-52'/>
+            <Image src={imgFour} alt='' className='absolute w-[50px] h-[50px] object-cover top-24 right-52'/>
+            <Image src={imgFive} alt='' className='absolute w-[50px] h-[50px] object-cover bottom-60 right-36'/>
+            <Image src={imgSix} alt='' className='absolute w-[50px] h-[50px] object-cover bottom-16 right-56'/>
+
+            <Swiper
+                modules={[Pagination]}
+                // style={{
+                //     "--swiper-pagination-bullet-active-width":"32px"
+                // }}
+                grabCursor={true}
+                initialSlide={1}
+                centeredSlides={true}
+                slidesPerView="auto"
+                speed={800}
+                pagination={{
+                    // el:".swiper-pagination",
+                    // type:"bullets",
+                    // clickable:true,
+                    // el:".swiper-pagination",
+                    // clickable:true,
+                    dynamicBullets:true,
+                    clickable:true
+                }}
+                breakpoints={{
+                    320:{spaceBetween:40},
+                    430:{spaceBetween:50},
+                    550:{spaceBetween:70},
+                    650:{spaceBetween:30},
+                }}
+            >
+
+                {/* First testimonial */}
+                <SwiperSlide className='swiper-slide slide-1 '>
+                    <div className='w-full h-[300px] bg-white rounded-2xl flex flex-col items-center justify-center shadow-xl relative'>
+                        <div className='flex w-[70%] flex-col gap-1 items-center justify-center'>
+                            <p className='tracking-wide text-black text-base'>Taskers are paid upon successfully completing tasks, ensuring that they are compensated for their efforts.</p>
+                            <h3 className='text-black text-sm tracking-wide mt-4'>David Beckham</h3>
+                            <p className='text-gray-400 text-xs tracking-wide'>Creative Director</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                {/* Second testimonial */}
+                <SwiperSlide className='swiper-slide slide-1 '>
+                    <div className='w-full h-[300px] bg-white rounded-2xl flex flex-col items-center justify-center shadow-xl'>
+                        <div className='flex w-[70%] flex-col gap-1 items-center justify-center'>
+                            <p className='tracking-wide text-black text-base'>Taskers are paid upon successfully completing tasks, ensuring that they are compensated for their efforts.</p>
+                            <h3 className='text-black text-sm tracking-wide mt-4'>David Beckham</h3>
+                            <p className='text-gray-400 text-xs tracking-wide'>Creative Director</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                {/* Third testimonial */}
+                <SwiperSlide className='swiper-slide slide-1 '>
+                    <div className='w-full h-[300px] bg-white rounded-2xl flex flex-col items-center justify-center shadow-xl'>
+                        <div className='flex w-[70%] flex-col gap-1 items-center justify-center'>
+                            <p className='tracking-wide text-black text-base'>Taskers are paid upon successfully completing tasks, ensuring that they are compensated for their efforts.</p>
+                            <h3 className='text-black text-sm tracking-wide mt-4'>David Beckham</h3>
+                            <p className='text-gray-400 text-xs tracking-wide'>Creative Director</p>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </div>
-        <Image src={imgOne} alt='' className='absolute w-[50px] h-[50px] object-cover bottom-16 left-56'/>
-        <Image src={imgTwo} alt='' className='absolute w-[50px] h-[50px] object-cover bottom-60 left-36'/>
-        <Image src={imgThree} alt='' className='absolute w-[50px] h-[50px] object-cover top-24 left-52'/>
-        <Image src={imgFour} alt='' className='absolute w-[50px] h-[50px] object-cover top-24 right-52'/>
-        <Image src={imgFive} alt='' className='absolute w-[50px] h-[50px] object-cover bottom-60 right-36'/>
-        <Image src={imgSix} alt='' className='absolute w-[50px] h-[50px] object-cover bottom-16 right-56'/>
-        
-        <Swiper 
-            modules={[Pagination]}
-            // style={{
-            //     "--swiper-pagination-bullet-active-width":"32px"
-            // }}
-            grabCursor={true}
-            initialSlide={1}
-            centeredSlides={true}
-            slidesPerView="auto"
-            speed={800}
-            pagination={{
-                // el:".swiper-pagination",
-                // type:"bullets",
-                // clickable:true,
-                // el:".swiper-pagination",
-                // clickable:true,
-                dynamicBullets:true,
-                clickable:true
-            }}
-            breakpoints={{
-                320:{spaceBetween:40},
-                430:{spaceBetween:50},
-                550:{spaceBetween:70},
-                650:{spaceBetween:30},
-            }}
-        >
-            
-            {/* First testimonial */}
-            <SwiperSlide className='swiper-slide slide-1 '>
-                <div className='w-full h-[300px] bg-white rounded-2xl flex flex-col items-center justify-center shadow-xl relative'>                          
-                    <div className='flex w-[70%] flex-col gap-1 items-center justify-center'>
-                        <p className='tracking-wide text-black text-base'>Taskers are paid upon successfully completing tasks, ensuring that they are compensated for their efforts.</p>
-                        <h3 className='text-black text-sm tracking-wide mt-4'>David Beckham</h3>
-                        <p className='text-gray-400 text-xs tracking-wide'>Creative Director</p>
-                    </div>
-                </div>                
-            </SwiperSlide>
-
-            {/* Second testimonial */}
-            <SwiperSlide className='swiper-slide slide-1 '>
-                <div className='w-full h-[300px] bg-white rounded-2xl flex flex-col items-center justify-center shadow-xl'>
-                    <div className='flex w-[70%] flex-col gap-1 items-center justify-center'>
-                        <p className='tracking-wide text-black text-base'>Taskers are paid upon successfully completing tasks, ensuring that they are compensated for their efforts.</p>
-                        <h3 className='text-black text-sm tracking-wide mt-4'>David Beckham</h3>
-                        <p className='text-gray-400 text-xs tracking-wide'>Creative Director</p>
-                    </div>
-                </div>                
-            </SwiperSlide>
-
-            {/* Third testimonial */}
-            <SwiperSlide className='swiper-slide slide-1 '>
-                <div className='w-full h-[300px] bg-white rounded-2xl flex flex-col items-center justify-center shadow-xl'>
-                    <div className='flex w-[70%] flex-col gap-1 items-center justify-center'>
-                        <p className='tracking-wide text-black text-base'>Taskers are paid upon successfully completing tasks, ensuring that they are compensated for their efforts.</p>
-                        <h3 className='text-black text-sm tracking-wide mt-4'>David Beckham</h3>
-                        <p className='text-gray-400 text-xs tracking-wide'>Creative Director</p>
-                    </div>
-                </div>                
-            </SwiperSlide>
-        </Swiper>
-    </div>
-  )
+    )
 }
 
 export default ViewReviews
