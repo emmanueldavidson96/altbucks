c306ec41bac3e93987fd911ca86922210bfc754a
Linet  Mukai <151999722+LinetMutuku@users.noreply.github.com>
2024-12-04 19:51:15 +0300

    Tasks updates

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -3,10 +3,12 @@
 import TasksHero from "../components/Tasks_Components/TasksHero";
 import TasksFilter from "../components/Tasks_Components/TasksFilter";
 import TasksList from "../components/Tasks_Components/TasksList";
+import Header from "../components/Dashboard_Components/Header"
 
 const TasksPage = () => {
     return (
         <div className="bg-white min-h-screen overflow-hidden">
+            <Header/>
             <TasksHero />
             <div className="container mx-auto px-6 py-8">
                 <div className="flex gap-8">
