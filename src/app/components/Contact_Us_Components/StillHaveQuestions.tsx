b3e73d7569aa045b91d1a29a10d03e7646c60ecb
dtechbro <princeakinola05@gmail.com>
2025-01-22 15:38:09 +0100

    done with responsiveness on landing, faq, about, contact, terms

--- src/app/components/Contact_Us_Components/StillHaveQuestions.tsx
+++ src/app/components/Contact_Us_Components/StillHaveQuestions.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function StillHaveQuestions() {
   return (
-    <div className='bg-white w-full h-fit pb-16 flex items-center justify-center '>
-        <div className='w-[70%] mx-auto flex justify-between items-center border py-8 px-6 border-gray-400 rounded-lg' >
-            <div className='flex flex-col gap-2'>
-                <h3 className='text-2xl font-semibold text-black tracking-wide'>Still have questions?</h3>
-                <p className='text-black text-sm tracking-wide '>
-                    Can't find the answer you're looking for? Please get in touch.
-                </p>
-            </div>
-            <button className='bg-[#2877EA] w-fit h-fit px-8 py-3 text-sm rounded-lg'>Get In Touch</button>
+    <div className="bg-white w-full pt-16 flex items-center justify-center ">
+      <div className="lg:w-[70%] mx-4 lg:mx-auto flex flex-col gap-5 lg:flex-row justify-between items-center border py-8 px-6 border-gray-400 rounded-lg">
+        <div className="flex flex-col gap-2">
+          <h3 className="text-2xl font-semibold text-black tracking-wide">
+            Still have questions?
+          </h3>
+          <p className="text-black text-sm tracking-wide ">
+            Can't find the answer you're looking for? Please get in touch.
+          </p>
         </div>
+
+        <button className="bg-[#2877EA] w-fit h-fit px-8 py-3 text-sm rounded-lg">
+          Get In Touch
+        </button>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Banner from '../components/Terms_of_Service_Component/Banner'
-import Header from '../components/Terms_of_Service_Component/Header'
-import AgreementToTerms from '../components/Terms_of_Service_Component/AgreementToTerms'
-import EarnRewards from '../components/Terms_of_Service_Component/EarnRewards'
-import Footer from '../components/Terms_of_Service_Component/Footer'
+import React from "react";
+import Banner from "../components/Terms_of_Service_Component/Banner";
+import AgreementToTerms from "../components/Terms_of_Service_Component/AgreementToTerms";
+import EarnRewards from "../components/Website_Shared_Components/EarnRewards";
+import Footer from "../components/Website_Shared_Components/Footer";
+import Navbar from "../components/Website_Shared_Components/Navbar";
 
 export default function page() {
   return (
-    <div className='w-screen h-fit bg-white'>
-        <Header />
-        <Banner />
-        <AgreementToTerms />
-        <EarnRewards />
-        <Footer />
+    <div className="w-screen h-fit bg-white">
+      <Navbar />
+      <Banner />
+      <AgreementToTerms />
+      <EarnRewards />
+      <Footer />
     </div>
-  )
+  );
 }
